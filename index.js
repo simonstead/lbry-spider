aafee9c8bd2321f398b6d6270eeadbed39bad0bb
Simon Stead <simonstead@me.com>
2017-05-22 08:45:39 +0100

    now pass in nout, will start to save claim data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const lbry = require('lbry-nodejs');
+const fs = require('fs');
 
 // lbry.claim_show("", {
 //   "txid": "9a33cd5da9d3f21e1e9633b9222e4e79200996a3745e4521d976797118d4f357",
@@ -17,38 +18,45 @@ lbry.block_show(current_blockhash)
   var l = transactions.length
   for (var i = 0; i < l; i++) {
     let txid = transactions[i]
-    claim_by_transaction(txid)
+    claim_by_transaction_nout(txid, 0)
   }
 })
 .catch((e) => console.log(e))
 
-// TODO: replace nout = 0 with `while it doesn't return false`
-const claim_by_transaction = (txid) => lbry.claim_show("", {
+const claim_by_transaction_nout = (txid, nout) => lbry.claim_show("", {
   "txid": txid,
-  "nout": 0
+  "nout": nout
 })
 .then((data) => {
-  const claim_data = data.result
   if (claim_data) {
-    const claim = {
-      name: claim_data.name,
-      channel_name: claim_data.channel_name,
-      claim_id: claim_data.claim_id,
-      txid: claim_data.txid,
-      nout: claim_data.nout,
-      isFree: !Boolean(claim_data.value.stream.metadata.fee)
-    }
-    Object.assign(claim, claim_data.value.stream.metadata)
-    if (claim.isFree) {
-      console.log("\n\nThis is a free download");
-    } else {
-      console.log("\n This costs", claim.fee.amount, claim.fee.currency);
-    }
-    // save_claim_data(claim_data);
-    //do we want the rest, like height, has signature, etc?
+    process_claim_data(data.result)
   } else {
-    console.log("\n\nProcessed 1 transaction. There was no claim associated with this txid");
-    // skip to next? do nothing?
+    console.log("\n\nProcessed 1 transaction/nout combo but there was no claim associated.");
   }
 })
 .catch((e) => console.log(e))
+
+const process_claim_data = (claim_data) => {
+    // const claim = {
+    //   name: claim_data.name,
+    //   channel_name: claim_data.channel_name,
+    //   claim_id: claim_data.claim_id,
+    //   txid: claim_data.txid,
+    //   nout: claim_data.nout,
+    //   isFree: !Boolean(claim_data.value.stream.metadata.fee)
+    // }
+    // Object.assign(claim, claim_data.value.stream.metadata)
+    //
+    // if (claim.isFree) {
+    //   console.log("\n\nThis is a free download");
+    // } else {
+    //   console.log("\n This costs", claim.fee.amount, claim.fee.currency);
+    // }
+    save_claim(claim_data);
+    //do we want the rest, like height, has signature, etc?
+}
+
+const save_claim = (claim) => {
+  const claim_string = JSON.stringify(claim)
+  fs.appendFile('./data/claims.json', claim)
+}
